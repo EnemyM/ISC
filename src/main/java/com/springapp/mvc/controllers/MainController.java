1cfe79d86a24a4cf6a478ffb1f393eb86290cb1c
EnemyM <fishing94@mail.ru>
2016-01-02 22:47:48 +0200

    Add: models, dao, refactor controller packed, rename main page.

--- src/main/java/com/springapp/mvc/controllers/MainController.java
+++ src/main/java/com/springapp/mvc/controllers/MainController.java
@@ -1,4 +1,4 @@
-package com.springapp.mvc;
+package com.springapp.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloController {
+public class MainController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("title", "BestDev");
 		model.addAttribute("h1", "Hello ISC !");
 		model.addAttribute("h2", "I will be great project!");
 		model.addAttribute("h3", "Information control system of delivery");
-		return "hello";
+		return "main";
 	}
 }
